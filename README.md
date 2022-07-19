d780698a7b7951a0e90d1662ba797631aacc9c2a
201b153 <tomarm698@gmail.com>
2022-07-19 22:29:03 +0530

    Adding First Insta Bot

---
+++ README.md
@@ -0,0 +1,4 @@
+#Insta-Chat-Bot
+->This Bot uses Intsa.js by @androz209. <br>
+->Try using Replit for better utility.<br>
+##Thank You
---
+++ index.js
@@ -0,0 +1,23 @@
+const Insta = require('./insta.js');
+const client = new Insta.Client();
+const chatbot = require("node-fetch").default;
+
+client.on('connected', () => {
+    console.log(`${client.user.username} Is Ready Now For Chats`);
+});
+
+client.on('messageCreate', (message) => {
+    if (message.author.id === client.user.id) return
+    message.markSeen();
+
+    if(message.content.toLowerCase().includes('hi')){ 
+        return message.chat.sendMessage('@mstomar698...LOADING');
+    } else
+    chatbot(`https://brv-chat.vercel.app/api?message=${encodeURIComponent(message.content)}`)
+    .then(res => res.json())
+    .then(json => {
+      message.chat.sendMessage(json.reply);
+    }).catch(err => {});
+});
+
+client.login('process.env.username', 'process.env.passwd');
